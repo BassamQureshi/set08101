a11d8f37307c386eeb1201556a4953f927c4619c
BassamQureshi <qureshibassam@gmail.com>
2023-03-11 16:47:08 +0000

    https://bassamqureshi.github.io/set08101/

---
+++ script.js
@@ -0,0 +1,17 @@
+const navbar = document.querySelector('.navbar');
+const darkModeButton = document.querySelector('.dark-mode-button');
+
+// Toggle dark mode
+darkModeButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  navbar.classList.toggle('dark-mode');
+});
+
+// Change navbar background color on scroll
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scroll');
+  } else {
+    navbar.classList.remove('scroll');
+  }
+});
